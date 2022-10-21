de2cef1507e4b6d1b1d7963118a38731f3cd86cd
Faldiansyah <faldiansyahk@gmail.com>
2022-10-21 19:05:06 +0800

    Initial commit

--- app/src/main/java/com/cowok/hijrah/chapter6topic4mvvm/model/getAllCar.kt
+++ app/src/main/java/com/cowok/hijrah/chapter6topic4mvvm/model/getAllCar.kt
@@ -1,6 +1,3 @@
 package com.cowok.hijrah.chapter6topic4mvvm.model
 
-
-import com.google.gson.annotations.SerializedName
-
 class getAllCar : ArrayList<getAllCarItem>()
--- app/src/main/java/com/cowok/hijrah/chapter6topic4mvvm/view/MainActivity.kt
+++ app/src/main/java/com/cowok/hijrah/chapter6topic4mvvm/view/MainActivity.kt
@@ -2,11 +2,37 @@ package com.cowok.hijrah.chapter6topic4mvvm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.cowok.hijrah.chapter6topic4mvvm.R
+import com.cowok.hijrah.chapter6topic4mvvm.databinding.ActivityMainBinding
+import com.cowok.hijrah.chapter6topic4mvvm.viewmodel.ViewModelCar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: ViewModelCar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        showDataCar()
+    }
+
+    private fun showDataCar() {
+        viewModel = ViewModelProvider(this).get(ViewModelCar::class.java)
+        viewModel.getliveDataCar().observe(this, Observer {
+            if (it != null) {
+                binding.rvCar.layoutManager = LinearLayoutManager(
+                    this, LinearLayoutManager.VERTICAL,false
+                )
+                binding.rvCar.adapter = CarAdapter(it)
+                val adapter = CarAdapter(it)
+                adapter.notifyDataSetChanged()
+            }
+        })
+        viewModel.callApiCar()
     }
 }
